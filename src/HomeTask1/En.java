c11e897512b3f53a4bdd5c57c6f9c21f40149f90
Radosław <radoslaw.lukasz.zegzula@gmail.com>
2022-10-29 13:49:00 +0200

    Update

---
+++ src/HomeTask1/En.java
@@ -0,0 +1,4 @@
+package HomeTask1;
+
+public enum En {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import Other.FuelType;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+        var a = 1;
+        var tr = FuelType.GAZ;
     }
 }
